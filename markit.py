ef3607cd073b497ee7fc49ddc38bbc7a7614ee1c
Saaleh Ijaz <aipyfusion@gmail.com>
2025-09-07 13:04:02 +0500

    updated some comments and imported libraries

--- markit.py
+++ markit.py
@@ -1,5 +1,20 @@
 from PIL import Image, ImageDraw, ImageFont
 import customtkinter as ctk
 
+def main():
 
+    # First Learn GUI with Tkinter.
+    # Maybe design a structure with Canva or maybe other more effective free website available for it.
+    # Make the logical structure and decide which functions to add.
+    # Draw programs logic and structure on paper or tool.
+    # Make prtotypes of those functions and classes with notes.
+    # Finally make full sudo code design of the program.
+    # Make the final code.
+    # Make unit tests accordingly.
+    # Make the documentation for it and comments too.
+    # Test it and Submit it as Final Project of CS50P.
+
+
+if __name__ == "__main__":
+    main()    # Initialize the main window
 
--- test_markit.py
+++ test_markit.py
@@ -1,1 +1,4 @@
+import pytest
 import markit
+
+# Here i will do unit testing to see everying working as i intended them to be.
