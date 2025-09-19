62bbb7c16d0ba3fd1ce16b871196403a6f99a538
Saaleh Ijaz <aipyfusion@gmail.com>
2025-09-19 11:07:12 +0500

    Add docstrings to file selection utility functions
    Added descriptive docstrings to png_selector, final_folder, and img_list functions in files_utils.py to clarify their purpose and usage for file and folder selection dialogs.

--- files_utils.py
+++ files_utils.py
@@ -9,17 +9,28 @@ input_imgs_list = []
 
 
 def png_selector():
+    """Open filemanager and help user select png file to
+        use as logo/watermark and save it in the global variable."""
+
     global png_file
+
     png_file = filedialog.askopenfilename(filetypes=[("PNG Images", "*.png")])
 
 
 def final_folder():
+    """Open filemanager to help user select the folder to save images after
+        bulk processing and save that path in global variable."""
+
     global output_folder
+
     output_folder = filedialog.askdirectory()
 
 
 def img_list():
+    """Validate all the supported images and save them in the global variable list."""
+
     global input_imgs_list
+    
     while True:
         folder = filedialog.askdirectory()
         input_imgs_list.clear()
